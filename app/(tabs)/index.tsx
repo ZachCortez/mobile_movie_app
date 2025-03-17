ac89fcfb29ac250900b904cd67ef408c2039b3fd
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2025-03-17 15:57:47 -0400

    Add files via upload

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,12 @@
 import { ScrollView, View, Image } from "react-native";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
+import React from "react";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+    const router = useRouter();
   return (
     <View className="flex-1 bg-primary">
         <Image source={images.bg}
@@ -12,6 +16,13 @@ export default function Index() {
         showsHorizontalScrollIndicator={false} contentContainerStyle={{
             minHeight: "100%", paddingBottom: 10}}>
             <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+
+            <View className="flex-1 mt-5">
+                <SearchBar
+                    onPress={() => router.push("/search")}
+                    placeholder="Search for a movie"
+                />
+            </View>
         </ScrollView>
     </View>
   );
