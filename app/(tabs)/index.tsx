82df18f25ad9f65d7786ef36b779b5ec6f19605b
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2025-03-17 15:24:15 -0400

    Add files via upload

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { ScrollView, View, Image } from "react-native";
 import {images} from "@/constants/images";
+import {icons} from "@/constants/icons";
 
 export default function Index() {
   return (
@@ -7,8 +8,10 @@ export default function Index() {
         <Image source={images.bg}
                className="absolute w-full z-0" />
 
-        <ScrollView>
-            <Image />
+        <ScrollView className="flex-1 px-5"
+        showsHorizontalScrollIndicator={false} contentContainerStyle={{
+            minHeight: "100%", paddingBottom: 10}}>
+            <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
         </ScrollView>
     </View>
   );
